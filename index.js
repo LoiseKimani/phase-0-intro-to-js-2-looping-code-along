6d4abaf804df20fc48880fae0eb7e42260bcfdcc
Loise <loise.kimani@student.moringaschool.com>
2022-11-30 10:07:51 +0300

    mid lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+
+function writeCards(names,event) {
+let newArray=[];
+
+for (let i=0; 1 <names.length; i++) {
+    let eventMessage= 'Thank you, ${names[i]} ,for the wonderful ${event} gift!';
+    newArray.push(eventMessage)
+}
+return newArray
+}
